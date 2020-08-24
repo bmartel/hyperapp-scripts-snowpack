c40847ed909bbce1a87c3703b29763a6ec72a956
Brandon Martel <brandonmartel@gmail.com>
2020-08-24 17:11:01 -0500

    initial commit

---
+++ snowpack.config.js
@@ -0,0 +1,16 @@
+const fs = require("fs");
+const path = require("path");
+
+const cwd = process.cwd();
+const isTS = fs.existsSync(path.join(cwd, "tsconfig.json"));
+
+module.exports = {
+  mount: {
+    public: "/",
+    src: "/_dist_",
+  },
+  plugins: ["@snowpack/plugin-babel", "@snowpack/plugin-dotenv"],
+  installOptions: {
+    installTypes: isTS,
+  },
+};
